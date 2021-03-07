0aae1ad83536e5cc7666acbbe88fef74e5c68c19
ixiaohu <631751599@qq.com>
2021-03-07 16:41:13 +0800

    sort_reads.c: Five long integers to hash a read (slow).

--- sort_reads.c
+++ sort_reads.c
@@ -20,7 +20,12 @@ typedef struct {
 } read_t;
 
 typedef kvec_t(read_t) read_v;
-#define read_lt(a, b) (((a).hash_v) < ((b).hash_v) || (strcmp((a).seq.seq, (b).seq.seq) < 0))
+#define read_lt(a, b) (((a).hash_v[0]) < ((b).hash_v[0]) \
+	|| ((a).hash_v[1]) < ((b).hash_v[1])                          \
+	|| ((a).hash_v[2]) < ((b).hash_v[2])                          \
+	|| ((a).hash_v[3]) < ((b).hash_v[3])                          \
+	|| ((a).hash_v[4]) < ((b).hash_v[4])                          \
+	|| (strcmp((a).seq.seq, (b).seq.seq) < 0))
 KSORT_INIT(read, read_t, read_lt)
 
 static uint64_t base2num(char x) {
@@ -32,17 +37,14 @@ static uint64_t base2num(char x) {
 }
 
 static void set_hashv(int len, const char *a, uint64_t v[]) {
-	int i; uint64_t ret = 1;
+	int i, j = 0;
 	// The len should less 160 stored five 64-bit number
 	for(i = 0; i < len; i++) {
-		ret += base2num(a[i]);
-		ret <<= 2U;
+		v[j] += base2num(a[i]);
+		v[j] <<= 2U;
+		if((i+1) % 32 == 0) j++;
 	}
 }
-
-typedef kvec_t(uint64_t) uint64_v;
-#define uint64_lt(a, b) ((a) < (b))
-KSORT_INIT(uint64, uint64_t, uint64_lt)
 
 int sort_reads_main(int argc, char *argv[]) {
 	if(argc != 4) return usage();
@@ -51,23 +53,24 @@ int sort_reads_main(int argc, char *argv[]) {
 		fprintf(stderr, "Open %s files\n", argv[1]);
 		return 1;
 	}
-	read_t tmp;
 
-	uint64_v p; kv_init(p);
-	do {
+	read_v reads; kv_init(reads);
+	read_t tmp;
+	while(1) {
 		tmp.seq = hfastq_fetch1(fi);
+		if(tmp.seq.l_seq == 0) break;
 		memset(tmp.hash_v, 0, sizeof(tmp.hash_v));
 		set_hashv(tmp.seq.l_seq, tmp.seq.seq, tmp.hash_v);
-		kv_push(uint64_t, p, tmp.hash_v[0]);
-	} while(tmp.seq.l_seq > 0);
-	ks_introsort(uint64, p.n, p.a);
-	int i, hit = 0;
-	for(i = 1; i < p.n; i++) {
-		if(p.a[i] == p.a[i-1]) {
-			hit++;
+		kv_push(read_t, reads, tmp);
+	}
+
+	ks_introsort(read, reads.n, reads.a);
+	int i, cnt = 0;
+	for(i = 1; i < reads.n; i++) {
+		if(strcmp(reads.a[i-1].seq.seq, reads.a[i].seq.seq) <= 0) {
+			cnt++;
 		}
 	}
-	fprintf(stderr, "Hit_ratio[%%] %.2f\n", 100.0 * hit / p.n);
-
+	printf("[%d, %ld]", cnt+1, reads.n);
 	return 0;
 }
