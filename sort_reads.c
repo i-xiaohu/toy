9e6c7de572123029db22f3719926d185661f76ae
ixiaohu <631751599@qq.com>
2021-03-08 16:36:21 +0800

    sort_reads.c: Cost too much memory.

--- sort_reads.c
+++ sort_reads.c
@@ -3,7 +3,6 @@
 //
 
 #include <stdio.h>
-#include <stdint.h>
 
 #include "hfastq.h"
 #include "kvec.h"
@@ -14,10 +13,11 @@ static int usage() {
 }
 
 typedef kvec_t(int) int_v;
+int_v srt_rid;
 
 typedef struct {
 	int id; // -1 means not end of a read.
-	int_v more_ids; // todo: 如果有更多匹配项，遍历之后请释放这里的内存
+	int_v more_ids;
 	long sons[5];
 } trie_node_t;
 typedef kvec_t(trie_node_t) trie_node_v;
@@ -37,13 +37,17 @@ static trie_node_t new_node(int id) {
 static void insert_string(trie_node_v *nodes, int id, int len, const char *s) {
 	int i;
 	long p = 0;
-	fprintf(stderr, "%s %d\n", s, len);
 	for(i = 0; i < len; i++) {
-		int c = 4;
+		int c;
 		if(s[i] == 'A') c = 0;
 		else if(s[i] == 'C') c = 1;
 		else if(s[i] == 'G') c = 2;
-		else if(s[i] == 'T') c = 3;
+		else if(s[i] == 'N') c = 3;
+		else if(s[i] == 'T') c = 4;
+		else {
+			fprintf(stderr, "[%s] Do not recognize the character '%c'\n", __func__, s[i]);
+			abort();
+		}
 		long child = nodes->a[p].sons[c];
 		if(child == -1) {
 			child = nodes->n;
@@ -56,19 +60,21 @@ static void insert_string(trie_node_v *nodes, int id, int len, const char *s) {
 				else kv_push(int, nodes->a[child].more_ids, id);
 			}
 		}
-		fprintf(stderr, "[%ld] -> [%ld] [%d] [%d, %c]\n", p, child, nodes->a[child].id, c, s[i]);
 		p = child;
 	}
 }
 
+int same_cnt;
 static void dump_sorted_reads(long fa) {
 	const trie_node_t *p = &trie_nodes.a[fa];
 	int i;
 	if(p->id != -1) {
-		printf("%s\n", reads.a[p->id].seq);
+		kv_push(int, srt_rid, p->id);
+		same_cnt += p->more_ids.n;
 		for(i = 0; i < p->more_ids.n; i++) {
-			printf("%s\n", reads.a[p->more_ids.a[i]].seq);
+			kv_push(int, srt_rid, p->more_ids.a[i]);
 		}
+		if(p->more_ids.a) free(p->more_ids.a);
 	}
 	for(i = 0; i < 5; i++) {
 		long child = p->sons[i];
@@ -95,7 +101,35 @@ int sort_reads_main(int argc, char *argv[]) {
 		insert_string(&trie_nodes, reads.n, read.l_seq, read.seq);
 		kv_push(bseq1_t, reads, read);
 	}
+	fprintf(stderr, "Input %ld reads\n", reads.n);
+
+	kv_init(srt_rid);
+	same_cnt = 0;
 	dump_sorted_reads(0);
+	// Check the correctness of sorting.
+	int i;
+//	for(i = 1; i < srt_rid.n; i++) {
+//		const char* last  = reads.a[srt_rid.a[i-1]].seq;
+//		const char* now = reads.a[srt_rid.a[i]].seq;
+//		if(strcmp(last, now) > 0) {
+//			fprintf(stderr, "%s\n", last);
+//			fprintf(stderr, "%s\n", now);
+//			fprintf(stderr, "Sorting fatal error.\n");
+//			return 1;
+//		}
+//	}
+//	fprintf(stderr, "The reads are sorted correctly.\n");
+	fprintf(stderr, "Found %d same reads\n", same_cnt);
+
+	// Free allocated memory.
+	for(i = 0; i < reads.n; i++) {
+		const bseq1_t *p = &reads.a[i];
+		free(p->name); free(p->comment); free(p->seq); free(p->qual);
+	}
+	free(reads.a);
+	free(trie_nodes.a);
+	free(srt_rid.a);
+	hfastq_close(fi);
 
 	return 0;
 }
